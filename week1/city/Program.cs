9a091e0a747885d12e6da54cf91d600328127f21
Collin Van der Vorst <collinvandervorst@LAPTOP-5O1LE2LM.guests.thomasmore.be>
2025-02-26 07:57:26 +0100

    Week 1

---
+++ week1/city/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MyAppNamespace;
+
+internal class Program
+{
+    private static void Main()
+    {
+        /*
+            I: Gent
+            O: You live in Gent
+        */
+        double nbr1 = Convert.ToDouble(Console.ReadLine());
+        double nbr2 = Convert.ToDouble(Console.ReadLine());
+        double nbr3 = Convert.ToDouble(Console.ReadLine());
+        double result = nbr1 + nbr2 + nbr3;
+
+        Console.WriteLine(Math.Floor(result));
+    }
+}
---
+++ week1/helloworld/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Week1HelloWorld;
+using System;
+using System.Formats.Tar;
+
+internal class Program
+{
+    private static void Main()
+    {
+        int n = Convert.ToInt16(Console.ReadLine());
+        char c = Convert.ToChar(n);
+
+
+        Console.WriteLine(Char.ToLower(c));
+        Console.WriteLine(Char.ToUpper(c));
+    }
+}

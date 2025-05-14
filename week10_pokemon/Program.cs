e77403efcd828b5a860c53647fcacd27c336c233
Collin Van der Vorst <collinvandervorst@DESKTOP-EARI8TH.eduroam.thomasmore.be>
2025-05-14 11:14:26 +0200

    Added pokemonballs

--- week10_pokemon/Program.cs
+++ week10_pokemon/Program.cs
@@ -9,5 +9,11 @@ internal class Program
         Pokemon pokemon2 = new Pokemon("Charizard", 200, 0.1);
         Pokemon pokemon3 = new Pokemon("Raichu", 300, 0.3);
         Pokemon pokemon4 = new Pokemon("Snorlax", 400, 1.0);
+
+        // Pokeballs
+        Pokeball ball1 = new Pokeball("Normal", 0.2);
+        Pokeball ball2 = new Pokeball("Special", 0.9);
+        Pokeball ball3 = new Pokeball("Advanced", 0.7);
+        Pokeball ball4 = new Pokeball("Normal", 0.2);
     }
 }
